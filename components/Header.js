18de909e86dc78b1a92e5fa802ab78cf114c9529
Raul Smith <raul.n.smith@gmail.com>
2023-01-02 11:58:54 -0700

    Privacy Policy page and CSS fixes

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <nav className="navbar navbar-expand-lg bg-light sticky-top border-bottom border-primary">
             <div className="container">
                 <Link href="/" className="navbar-brand">
-                    <img src="https://theblueroseaffiliation.com/img/blue-rose-logo.png" height="125" alt="The Blue Rose Affiliation" />
+                    <img src="https://theblueroseaffiliation.com/img/blue-rose-final-logo.png" height="125" alt="The Blue Rose Affiliation" />
                 </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -13,16 +13,16 @@ const Header = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0 justify-content-end">
                         <li className="nav-item px-1">
-                            <Link href="/" className="nav-link">Home</Link>
+                            <Link href="/" className="nav-link font-lg">Home</Link>
                         </li>
                         <li className="nav-item px-1">
-                            <Link href="/about" className="nav-link">About</Link>
+                            <Link href="/about" className="nav-link font-lg">About</Link>
                         </li>
                         <li className="nav-item px-1">
-                            <Link href="/contact" className="nav-link">Contact</Link>
+                            <Link href="/contact" className="nav-link font-lg">Contact</Link>
                         </li>
                         <li className="nav-item px-1">
-                            <Link href="/join" className="nav-link">How to Join</Link>
+                            <Link href="/join" className="nav-link font-lg">How to Join</Link>
                         </li>
                     </ul>
                 </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect } from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 import '../styles/owl.css';
 
 function MyApp({ Component, pageProps }) {
@@ -10,13 +11,15 @@ function MyApp({ Component, pageProps }) {
   }, [])
 
   return (
-    <div className="container-fluid pe-0">
-      <div className="row">
-        <div className="col px-0">
+    <div className="container-fluid px-0">
+      <div className="row mx-0">
+        <div className="col px-0 pb-5 mb-5">
           <main className="main">
             <Header />
             <Component {...pageProps} />
+            <Footer />
           </main>
+          <div>&nbsp;</div>
         </div>
       </div>
     </div>
