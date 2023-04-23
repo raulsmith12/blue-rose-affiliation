5a8ca38cf5fe64e2d0738defde1388eeba8657d7
Raul Smith <raul.n.smith@gmail.com>
2023-04-22 23:02:49 -0600

    Mobile fix

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,11 @@
 import Link from "next/link";
 
 const Header = () => {
+    function hideNavbar() {
+        const element = document.getElementById('navbarSupportedContent');
+        element.classList.remove('show');
+    }
+
     return (
         <nav className="navbar navbar-expand-lg bg-light sticky-top border-bottom border-primary">
             <div className="container">
@@ -13,16 +18,16 @@ const Header = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0 justify-content-end">
                         <li className="nav-item px-1">
-                            <Link href="/" className="nav-link font-lg text-shadow-sm text-primary">Home</Link>
+                            <Link href="/" className="nav-link font-lg text-shadow-sm text-primary" onClick={hideNavbar}>Home</Link>
                         </li>
                         <li className="nav-item px-1">
-                            <Link href="/about" className="nav-link font-lg text-shadow-sm text-primary">About</Link>
+                            <Link href="/about" className="nav-link font-lg text-shadow-sm text-primary" onClick={hideNavbar}>About</Link>
                         </li>
                         <li className="nav-item px-1">
-                            <Link href="/contact" className="nav-link font-lg text-shadow-sm text-primary">Contact</Link>
+                            <Link href="/contact" className="nav-link font-lg text-shadow-sm text-primary" onClick={hideNavbar}>Contact</Link>
                         </li>
                         <li className="nav-item px-1">
-                            <Link href="/join" className="nav-link font-lg text-shadow-sm text-primary">How to Join</Link>
+                            <Link href="/join" className="nav-link font-lg text-shadow-sm text-primary" onClick={hideNavbar}>How to Join</Link>
                         </li>
                     </ul>
                 </div>
